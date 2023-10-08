35d383d8bb195252aa3edccaff79216114b65645
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2023-10-08 18:07:18 +0500

    added footer

---
+++ src/components/Footer/FooterIcons/index.jsx
@@ -0,0 +1,7 @@
+
+import Instagram from "./Instagram";
+import Telegram from "./Telegram";
+import YouTube from "./YouTube";
+import Facebook from "./Facebook";
+
+export { Instagram, Telegram, YouTube, Facebook };
